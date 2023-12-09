a7fe3bc609e3b571ed4d82dc61ed22a563c671d8
Your Name <you@example.com>
2023-12-09 16:55:26 +0100

    n

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="BigEDA",
-    version="0.0.1",
+    version="0.0.2",
     author="Fabio Scielzo Ortiz",
     author_email="fabioscielzo98@gmail.com",
     description="This is a package to carry out Exploratory Data Analysis, allowing to work on Big Data.",
