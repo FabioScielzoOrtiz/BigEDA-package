37a204030304425cc1310adc92a7a503018c3ded
Your Name <you@example.com>
2024-05-18 11:10:46 +0200

    n

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['pandas','numpy', 'polars', 'scipy', 'itertools', 
+    install_requires=['pandas','numpy', 'polars', 'scipy',  
                       'collections', 'seaborn', 'matplotlib', 'random'],
     python_requires=">=3.7"
 )
