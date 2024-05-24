8a1d410d8e966808284c6b7f92c6ca07b4cc45d7
Your Name <you@example.com>
2024-05-24 12:40:30 +0200

    n

--- BigEDA/plots.py
+++ BigEDA/plots.py
@@ -794,7 +794,7 @@ def barplot_matrix(df, n_cols, title, figsize=(15,15), auto_col=False,
 
 def barplot_interactive(X, figsize=(800,600), font_family='Comic Sans MS', 
                         xlabel_size=12, ylabel_size=12, xticks_size=10, yticks_size=10, 
-                        color='tomato', categories_order=None,
+                        color='tomato', categories_order=None, orientation='h',
                         margin_l=50, margin_r=40, margin_t=60, margin_b=50, 
                         title=None, title_size=20, title_width=0.5, title_height=1.08):
 
@@ -807,10 +807,16 @@ def barplot_interactive(X, figsize=(800,600), font_family='Comic Sans MS',
     rel_freq_perc = np.round(rel_freq*100, 2)
     df_to_plot = pd.DataFrame({'Value': unique_values, 'Percentage': rel_freq_perc})
 
-    if categories_order is not None:
-        fig = px.bar(df_to_plot, y='Value', x='Percentage', category_orders={"Value": categories_order})
-    else:
-        fig = px.bar(df_to_plot, y='Value', x='Percentage')
+    if orientation == 'h':
+        if categories_order is not None:
+            fig = px.bar(df_to_plot, y='Value', x='Percentage', category_orders={"Value": categories_order})  
+        else:
+            fig = px.bar(df_to_plot, y='Value', x='Percentage')
+    elif orientation == 'v':
+        if categories_order is not None:
+            fig = px.bar(df_to_plot, x='Value', y='Percentage', category_orders={"Value": categories_order})  
+        else:
+            fig = px.bar(df_to_plot, x='Value', y='Percentage')
 
     if title is None:
         title = f'<b>Barplot - {X.name}<b>'
@@ -1061,6 +1067,147 @@ def scatterplot_matrix(df, n_cols, title, figsize=(15,15), auto_col=False,
         fig.savefig(file_name + '.jpg', format='jpg', dpi=500)
 
     plt.show()  
+
+######################################################################################################################
+
+def scatterplot_interactive(df, x, y, z_size=None, z_color=None, point_labels=None, hover_name=None, 
+                        figsize=(800,600), font_family='Comic Sans MS', 
+                        xlabel=None, xlabel_size=12, ylabel_size=12, 
+                        xticks_size=10, yticks_size=10, legend_title_side='top',
+                        color='tomato', point_size=10, colorscale='Cividis',
+                        margin_l=50, margin_r=40, margin_t=60, margin_b=50, 
+                        title=None, title_size=20, title_width=0.5, title_height=1.08,
+                        point_labels_size=10, point_labels_position='top center'):
+    
+    fig = px.scatter(df, x=x, y=y, size=z_size, hover_name=hover_name, color=z_color, text=point_labels)
+
+    if title is None:
+        title = f'<b>Scatterplot - {y} vs {x}<b>'
+
+    # Adjust the plot size
+    fig.update_layout(
+        width=figsize[0],  # width of the plot in pixels
+        height=figsize[1]  # height of the plot in pixels
+    )
+
+    # Update layout for axis titles
+    fig.update_layout(
+        xaxis_title=dict(
+            text=xlabel if xlabel is not None else x,
+            font=dict(
+                family=font_family,
+                size=xlabel_size,
+                color="black"
+            )
+        ),
+        yaxis_title=dict(
+            text=y,
+            font=dict(
+                family=font_family,
+                size=ylabel_size,
+                color="black"
+            )
+        )
+    )
+
+    fig.update_layout(
+        annotations=[
+            dict(
+                text=title,
+                x=title_width,
+                y=title_height,
+                xref='paper',
+                yref='paper',
+                showarrow=False,
+                font=dict(
+                    family=font_family,
+                    size=title_size,
+                    color="black"
+                )
+            )
+        ]
+    )
+
+    fig.update_layout(
+        margin=dict(l=margin_l, r=margin_r, t=margin_t, b=margin_b)
+    )
+
+    fig.update_layout(
+        plot_bgcolor='white'
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        tickfont=dict(
+            family=font_family,
+            size=xticks_size,
+            color='black'
+        )
+    )
+
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        automargin=True,
+        title_standoff=20,  # Increase this value to add more space between y-axis label and ticks
+        tickfont=dict(
+            family=font_family,
+            size=yticks_size,
+            color='black'
+        )
+    )
+
+    # Add hover label styling
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            bordercolor="black",
+            font_size=13,
+            font_family=font_family,
+            font_color="black"
+        )
+    )
+    
+    # Apply color scale if z_color is specified
+    if z_color:
+        fig.update_layout(
+            coloraxis_colorbar=dict(
+                title=dict(
+                    text=z_color,
+                    font=dict(
+                        family=font_family,
+                        size=15,
+                        color='black'
+                    ),
+                    side=legend_title_side  # Position of the colorbar title
+                )
+            )
+        )
+
+        fig.update_layout(coloraxis_colorscale=colorscale)  
+    else:
+        # Apply default color and point size if z_color is not specified
+        fig.update_traces(marker=dict(color=color, size=point_size))
+
+    # Set text position on points
+    fig.update_traces(textposition=point_labels_position)
+    # Set text position, font, and size on points
+    fig.update_traces(
+        textposition=point_labels_position,
+        textfont=dict(
+            family=font_family,
+            size=point_labels_size,
+            color='black'
+        )
+    )
+    return fig
 
 ######################################################################################################################
 
@@ -1407,6 +1554,125 @@ def lineplot_interactive(df, x, y, figsize=(800,600), font_family='Arial',
         tick_vals = x_vals[::max(1, len(x_vals) // num_xticks)]
         fig.update_xaxes(tickvals=tick_vals)
 
+    return fig
+
+######################################################################################################################
+
+def lineplot_interactive_multiplot(dic_df, y, figsize=(800,600), font_family='Comic Sans MS', 
+                        xlabel_size=12, ylabel_size=12, xticks_size=10, yticks_size=10, 
+                        n_cols=2, wspace=0.5, hspace=0.5, subtitle_size=15, tickangle=90,
+                        margin_l=50, margin_r=40, margin_t=60, margin_b=50, line_width=3, color=None,
+                        title=None, title_size=20, title_width=0.5, title_height=1.08, nxticks=5):
+    
+    periods = dic_df.keys()
+    n_subplots = len(periods)
+    n_rows = int(np.ceil(n_subplots / n_cols))
+    colors = px.colors.qualitative.Plotly[:n_subplots] 
+    fig = make_subplots(rows=n_rows, cols=n_cols, shared_xaxes=False, shared_yaxes=False,
+                        subplot_titles=[f"{y} by {period}" for period in periods],
+                        vertical_spacing=hspace, horizontal_spacing=wspace)
+
+    for i, period in enumerate(periods):
+        df_to_plot = dic_df[period].to_pandas().sort_values(by=period)
+        line_fig = px.line(df_to_plot, x=period, y=y)
+        line_fig.update_traces(line_color=colors[i], line_width=line_width) if color is None else line_fig.update_traces(line_color=color, line_width=line_width) 
+        row = (i // n_cols) + 1
+        col = (i % n_cols) + 1
+        for trace in line_fig['data']:
+            fig.add_trace(trace, row=row, col=col)
+
+        # Customize x-axis tick format
+        fig.update_xaxes(title_text=period, row=row, col=col, title_font=dict(family=font_family, size=xlabel_size, color="black"),
+                         tickangle=tickangle[period], tickfont=dict(family=font_family, size=xticks_size, color='black'),
+                         tickformat='%Y-%m-%d', nticks=nxticks[period])
+        fig.update_yaxes(title_text=y, row=row, col=col, title_font=dict(family=font_family, size=ylabel_size, color="black"))
+
+        # Remove the y-axis labels for the right subplots
+        if col > 1:
+            fig.update_yaxes(title_text='', row=row, col=col)
+        if row < n_rows:
+            fig.update_xaxes(title_text='', row=row, col=col)
+
+    if title is None:
+        title = f'<b>Multiple Times Series - {y}<b>'
+
+    # Adjust the plot size
+    fig.update_layout(
+        width=figsize[0],  # width of the plot in pixels
+        height=figsize[1],  # height of the plot in pixels
+        margin=dict(l=margin_l, r=margin_r, t=margin_t, b=margin_b)  # increase top margin for title
+    )
+
+    # Ensure subplot titles are not overridden
+    annotations = list(fig['layout']['annotations'])
+    annotations.append(
+        dict(
+            text=title,
+            x=title_width,
+            y=title_height, # Position above the subplots
+            xref='paper',
+            yref='paper',
+            showarrow=False,
+            font=dict(
+                family=font_family,
+                size=title_size,
+                color="black"
+            )
+        )
+    )
+    fig.update_layout(annotations=annotations)
+    
+    # Explicitly update subplot title sizes
+    for annotation in fig['layout']['annotations']:
+        for period in periods:
+            if 'text' in annotation and period in annotation['text']:
+                annotation['font']['size'] = subtitle_size
+                annotation['font']['family'] = font_family
+                annotation['font']['color'] = 'black'
+
+    fig.update_layout(
+        plot_bgcolor='white'
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        tickfont=dict(
+            family=font_family,
+            size=xticks_size,
+            color='black'
+        )
+    )
+
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='white',
+        automargin=True,
+        title_standoff=20,
+        tickfont=dict(
+            family=font_family,
+            size=yticks_size,
+            color='black'
+        )
+    )
+
+    # Add hover label styling
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            bordercolor="black",
+            font_size=12,
+            font_family=font_family,
+            font_color="black"
+        )
+    )
+    
     return fig
 
 ######################################################################################################################
@@ -2050,6 +2316,231 @@ def barplot_2D(df, cat_condition, cat_conditioned, n_rows, figsize, title_size, 
     for j in range(n_categories, n_rows * n_cols):
         fig.delaxes(axes[j])
     plt.show()
+
+##########################################################################################
+
+def barplot_interactive_2D(df, x, y, figsize=(800,600), font_family='Comic Sans MS', 
+                        xlabel=None, xlabel_size=12, ylabel_size=12, xticks_size=10, yticks_size=10, 
+                        color='tomato', 
+                        margin_l=50, margin_r=40, margin_t=60, margin_b=50, 
+                        title=None, title_size=20, title_width=0.5, title_height=1.08):
+    
+
+    fig = px.bar(df, x=x, y=y)
+
+    if title is None:
+        title = f'<b>Conditional Barplot - {x} | {y}<b>'
+
+    # Set the bar color 
+    fig.update_traces(marker_color=color)
+
+    # Adjust the plot size
+    fig.update_layout(
+        width=figsize[0],  # width of the plot in pixels
+        height=figsize[1]  # height of the plot in pixels
+    )
+
+    # Update layout for axis titles
+    fig.update_layout(
+        xaxis_title=dict(
+            text=xlabel if xlabel is not None else x,
+            font=dict(
+                family=font_family,
+                size=xlabel_size,
+                color="black"
+            )
+        ),
+        yaxis_title=dict(
+            text=y,
+            font=dict(
+                family=font_family,
+                size=ylabel_size,
+                color="black"
+            )
+        )
+    )
+
+    fig.update_layout(
+        annotations=[
+            dict(
+                text=title,
+                x=title_width,
+                y=title_height,
+                xref='paper',
+                yref='paper',
+                showarrow=False,
+                font=dict(
+                    family=font_family,
+                    size=title_size,
+                    color="black"
+                )
+            )
+        ]
+    )
+
+    fig.update_layout(
+        margin=dict(l=margin_l, r=margin_r, t=margin_t, b=margin_b)
+    )
+
+    fig.update_layout(
+        plot_bgcolor='white'
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        tickfont=dict(
+            family=font_family,
+            size=xticks_size,
+            color='black'
+        )
+    )
+
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='white',
+        automargin=True,
+        title_standoff=20,  # Increase this value to add more space between y-axis label and ticks
+        tickfont=dict(
+            family=font_family,
+            size=yticks_size,
+            color='black'
+        )
+    )
+
+    # Add hover label styling
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            bordercolor="black",
+            font_size=13,
+            font_family=font_family,
+            font_color="black"
+        )
+    )
+    
+    return fig
+
+##########################################################################################
+
+def barplot_interactive_2D_multiplot(df, x, y, figsize=(800,600), font_family='Comic Sans MS', 
+                        xlabel=None, xlabel_size=12, ylabel_size=12, xticks_size=10, yticks_size=10, 
+                        n_cols=2, wspace=0.5, hspace=0.5, subtitle_size=15,
+                        margin_l=50, margin_r=40, margin_t=60, margin_b=50, 
+                        title=None, title_size=20, title_width=0.5, title_height=1.08):
+    
+    y_categories = df[y].unique()
+    n_subplots = len(y_categories)
+    n_rows = int(np.ceil(n_subplots / n_cols))
+    colors = px.colors.qualitative.Plotly[:n_subplots] 
+    fig = make_subplots(rows=n_rows, cols=n_cols, shared_xaxes=False, shared_yaxes=False,
+                        subplot_titles=[f"{y} = {cat}" for cat in y_categories],
+                        vertical_spacing=hspace, horizontal_spacing=wspace)
+
+    for i, cat in enumerate(y_categories):
+        df_to_plot = df.filter(pl.col(y) == cat).to_pandas()
+        bar_fig = px.bar(df_to_plot, x='percentage', y=x)
+        bar_fig.update_traces(marker_color=colors[i])
+        row = (i // n_cols) + 1
+        col = (i % n_cols) + 1
+        for trace in bar_fig['data']:
+            fig.add_trace(trace, row=row, col=col)
+        fig.update_xaxes(title_text=xlabel if xlabel is not None else x, row=row, col=col, title_font=dict(family=font_family, size=xlabel_size, color="black"))
+        fig.update_yaxes(title_text=x, row=row, col=col, title_font=dict(family=font_family, size=ylabel_size, color="black"))
+
+        # Remove the y-axis labels for the right subplots
+        if col > 1:
+            fig.update_yaxes(title_text='', row=row, col=col)
+        if row < n_rows:
+            fig.update_xaxes(title_text='', row=row, col=col)
+
+    if title is None:
+        title = f'<b>Conditional Barplot - {x} | {y}<b>'
+
+    # Adjust the plot size
+    fig.update_layout(
+        width=figsize[0],  # width of the plot in pixels
+        height=figsize[1],  # height of the plot in pixels
+        margin=dict(l=margin_l, r=margin_r, t=margin_t + 40, b=margin_b)  # increase top margin for title
+    )
+
+    # Ensure subplot titles are not overridden
+    annotations = list(fig['layout']['annotations'])
+    annotations.append(
+        dict(
+            text=title,
+            x=title_width,
+            y=title_height,  # Position above the subplots
+            xref='paper',
+            yref='paper',
+            showarrow=False,
+            font=dict(
+                family=font_family,
+                size=title_size,
+                color="black"
+            )
+        )
+    )
+    fig.update_layout(annotations=annotations)
+    
+    # Explicitly update subplot title sizes
+    for annotation in fig['layout']['annotations']:
+        for cat in y_categories:
+            if 'text' in annotation and cat in annotation['text']:
+                annotation['font']['size'] = subtitle_size
+                annotation['font']['family'] = font_family
+                annotation['font']['color'] = 'black'
+
+    fig.update_layout(
+        plot_bgcolor='white'
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='lightgrey',
+        tickfont=dict(
+            family=font_family,
+            size=xticks_size,
+            color='black'
+        )
+    )
+
+    fig.update_yaxes(
+        mirror=True,
+        ticks='outside',
+        showline=True,
+        linecolor='black',
+        gridcolor='white',
+        automargin=True,
+        title_standoff=20,
+        tickfont=dict(
+            family=font_family,
+            size=yticks_size,
+            color='black'
+        )
+    )
+
+    # Add hover label styling
+    fig.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            bordercolor="black",
+            font_size=12,
+            font_family=font_family,
+            font_color="black"
+        )
+    )
+    
+    return fig
 
 ##########################################################################################
 
